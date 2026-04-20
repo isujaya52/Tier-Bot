bd40241b52f8331b074ffb32606f275539650bd7
isujaya52 <isujaya52@sma.belajar.id>
2026-04-21 07:12:43 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from telebot.async_telebot import AsyncTeleBot
+import os
+
+TOKEN = os.environ['TOKEN']
+LOGS_ID = os.environ['LOGS_ID']
+own = [5039288972, 1928677026]
+
+bot = AsyncTeleBot(TOKEN, parse_mode='html')
