b017b2f7ddffa7969d7d7ec2c1d3369f79357df0
isujaya52 <isujaya52@sma.belajar.id>
2026-04-22 12:29:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ from datetime import datetime
 from BOT.all_cmd import ping
 from BOT.all_cmd import get_readable_uptime
 
-# 1. Tambahkan variabel global untuk mencatat waktu mulai
-START_TIME = time.time()
 
 flask_app = Flask(__name__)
 main_loop = None
